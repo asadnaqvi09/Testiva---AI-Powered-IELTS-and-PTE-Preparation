b09cce179fbfdf4a6e62e8f1c2f9978f50b3f656
asad khan tanoli <nasad8569@gmail.com>
2026-03-11 23:35:44 +0500

    Remove some unwanted lines of code in users sql file

--- Backend/database/schema/users.sql
+++ Backend/database/schema/users.sql
@@ -12,29 +12,17 @@ CREATE TYPE subscription_status_enum AS ENUM (
 
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-
     email VARCHAR(255) UNIQUE NOT NULL,
-
     password_hash TEXT,
-
     full_name VARCHAR(150),
-
     avatar_url TEXT,
-
     country VARCHAR(100),
-
     auth_provider VARCHAR(50) DEFAULT 'email',
-
     role user_role_enum NOT NULL DEFAULT 'user',
-
     subscription subscription_status_enum NOT NULL DEFAULT 'free',
-
     is_email_verified BOOLEAN DEFAULT FALSE,
-
     last_login_at TIMESTAMP,
-
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
